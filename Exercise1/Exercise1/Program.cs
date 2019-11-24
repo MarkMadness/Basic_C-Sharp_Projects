7f30b5ee75e230ee38b00f1eb0875c9dd1200446
Mark <1187productions@gmail.com>
2019-11-24 09:42:13 -0700

    Uploaded Exercise 1 files

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're 'acounting' on you!");
+            Console.ReadLine();
+        }
+    }
+}
