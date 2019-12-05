c67e41e6d18858eb370e68af6bf0d7fb4994b76e
Mark <1187productions@gmail.com>
2019-12-05 11:39:01 -0700

    Add files via upload

---
+++ Exercise12/Exercise12/Operation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    public class Operation
+    {
+        public string MathOne(int i)
+        {
+            int answer = i + 1;
+            string giveAnswer = Convert.ToString(answer);
+            return giveAnswer;
+        }
+        public string MathTwo(int i)
+        {
+            int answer = i - 1;
+            string giveAnswer = Convert.ToString(answer);
+            return giveAnswer;
+        }
+        public string MathThree(int i)
+        {
+            int answer = i * 2;
+            string giveAnswer = Convert.ToString(answer);
+            return giveAnswer;
+        }
+    }
+}
---
+++ Exercise12/Exercise12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = new Operation();
+            Console.WriteLine("What number do you want to do the math operations on?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            string ansOne = op.MathOne(input);
+            string ansTwo = op.MathTwo(input);
+            string ansThree = op.MathThree(input);
+            
+            Console.WriteLine("Your number + 1 = " + ansOne);
+            Console.WriteLine("Your number - 1 = " + ansTwo);
+            Console.WriteLine("Your number x 2 = " + ansThree);
+            Console.ReadLine();
+        }
+    }
+}
