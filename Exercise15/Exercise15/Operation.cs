d300a7f1c3a593b4faa24a5bbe913830dd907c87
Mark <1187productions@gmail.com>
2019-12-09 12:32:23 -0700

    Uploaded Exercise 15 files

---
+++ Exercise15/Exercise15/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    class Operation
+    {
+        public int MathOp(int x, int y = 5)
+        {
+            int answer = x + y;
+            Console.WriteLine("Your first number is " + Convert.ToString(x) + ". The second number is " + 
+                Convert.ToString(y) + ". \n   (If you didn't input a second number, the default if 5)");
+            Console.WriteLine("The two numbers added together are " + Convert.ToString(answer) + ".");
+            return answer;
+        }
+    }
+}
---
+++ Exercise15/Exercise15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A second number is an optional. Would you like to type a second number?");
+            Console.WriteLine("    'Yes' or 'No'");
+            string response = Console.ReadLine();
+            Operation op = new Operation();
+
+            if (response == "Yes")
+            {
+                Console.WriteLine("Please enter the second number.");
+                int inputTwo = Convert.ToInt32(Console.ReadLine());
+                op.MathOp(x: inputOne, y: inputTwo);
+            }
+            else
+            {
+                op.MathOp(inputOne);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
