208df1d7709fbd8c0c990bb57ca7e1de8423e38a
Mark <1187productions@gmail.com>
2019-12-09 11:24:38 -0700

    Uploaded Exercise 13 files

---
+++ Exercise13/Exercise13/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Operation
+    {
+        public void MathOp(int x, int y)
+        {
+            int answer = x + 1;
+            Console.WriteLine("Your second input was " + Convert.ToString(y));
+        }
+
+        public void MathOp(int x)
+        {
+            Console.WriteLine("This is an overloaded method.");
+        }
+    }
+}
---
+++ Exercise13/Exercise13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = new Operation(); // instantiate the class
+            Console.WriteLine("Please enter a number.");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number.");
+            int inputTwo = Convert.ToInt32(Console.ReadLine());
+            
+            op.MathOp(inputOne, inputTwo); // calling the method passing the input numbers
+            op.MathOp(x: inputOne); // calling the method in the class, specifying the parameters by name
+
+            Console.ReadLine();
+        }
+    }
+}
