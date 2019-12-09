d3d611a7c9fa1332e3139159a5e5bbb6bef8611d
Mark <1187productions@gmail.com>
2019-12-09 12:00:44 -0700

    Uploaded Exercise 14 files

---
+++ Exercise14/Exercise14/Operation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise14
+{
+    class Operation
+    {
+        public int MathOp(int one)
+        {
+            int answer = one + 5;
+            Console.WriteLine("The answer for the first method is " + answer);
+            return answer;
+        }
+        public decimal MathOp(decimal two)
+        {
+            decimal a = 23.543m;
+            decimal answer = a + two;
+            Console.WriteLine("The answer for the second method is " + answer);
+            int ansGive = Convert.ToInt32(answer);
+            return ansGive;
+        }
+        public string MathOp(string three)
+        {
+            int numThree = Convert.ToInt32(three);
+            int answer = numThree + 7;
+            Console.WriteLine("The answer for the third method is " + answer);
+            string ansGive = Convert.ToString(answer);
+            return ansGive;
+        }
+    }
+}
---
+++ Exercise14/Exercise14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = 5;
+            decimal two = 34.5647m;
+            string three = "2";
+
+            Operation op = new Operation();
+
+            op.MathOp(one);
+            op.MathOp(two: two);
+            op.MathOp(three: three);
+
+            Console.ReadLine();
+        }
+    }
+}
