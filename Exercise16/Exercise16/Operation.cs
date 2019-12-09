c64685d8d4a3dfdf2709929760bf73154839b38c
Mark <1187productions@gmail.com>
2019-12-09 14:00:34 -0700

    Uploaded Exercise 16 files

---
+++ Exercise16/Exercise16/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Operation
+    {
+        public static void OutProvide(out int num)
+        {
+            num = 2;
+        }
+        public void MathOp(int x)
+        {
+            int num;
+            OutProvide(out num);
+            int answer = x / num;
+            int remain = x % num;
+            Console.WriteLine("Your number divided by " + num + " = " + answer + " with a remainder of " + remain);
+        }
+    }
+}
---
+++ Exercise16/Exercise16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = new Operation();
+
+            Console.WriteLine("Please enter a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            op.MathOp(x: input);
+
+            Console.ReadLine();
+        }
+    }
+}
