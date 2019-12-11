47f98021b4843778e18e3c10bc66583506b52dbb
Mark <1187productions@gmail.com>
2019-12-11 13:12:40 -0700

    Uploaded Exercise 18 files

---
+++ Exercise18/Exercise18/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise18
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("This is the Person Class - Method SayName(). This is the name {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Exercise18/Exercise18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em = new Employee();
+            em.firstName = "Sample";
+            em.lastName = "Student";
+            em.SayName();
+            Console.ReadLine();
+        }
+    }
+}
