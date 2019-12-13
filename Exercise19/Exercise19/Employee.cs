3890ec3b6731fc51defbe047de7c10d8a9e6b803
Mark <1187productions@gmail.com>
2019-12-13 12:46:39 -0700

    Upload Exercise 19 files

---
+++ Exercise19/Exercise19/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise19
+{
+    public class Employee: Person
+    {
+        public string iD { get; set; }
+        public static string operator ==(Employee a, Employee b)
+        {
+            if (a.iD == b.iD)
+            {
+                string status = "The IDs are equal to each other.";
+                return status;
+            }
+            else
+            {
+                string status = "The IDs are not equal to each other.";
+                return status;
+            }
+        }
+        public static string operator !=(Employee a, Employee b)
+        {
+            if (a.iD != b.iD)
+            {
+                string status = "The IDs are not equal to each other.";
+                return status;
+            }
+            else
+            {
+                string status = "The IDs are equal to each other.";
+                return status;
+            }
+        }
+    }
+}
---
+++ Exercise19/Exercise19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee em = new Employee();
+            Employee emp = new Employee();
+            em.iD = "1234";
+            emp.iD = "2345";
+
+            string status = em == emp;
+
+            Console.WriteLine(status);
+            Console.ReadLine();
+        }
+    }
+}
