cd1e1aab489e26451038d54c3636725f0736381b
Mark <1187productions@gmail.com>
2019-12-16 16:54:26 -0700

    Uploaded Exercise 20 files

---
+++ Exercise20/Exercise20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Week week = new Week();
+                Console.WriteLine("Please type the current day. Capitalize the first letter.");
+                string answer = Console.ReadLine();
+                Enum.Parse(typeof(DaysOfTheWeek), answer);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
