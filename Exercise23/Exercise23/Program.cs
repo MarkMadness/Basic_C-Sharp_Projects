bbfa240d78494954c6e3515b5852052a80f8569c
Mark <1187productions@gmail.com>
2019-12-29 09:32:57 -0700

    Uploaded Exercise 23 files

---
+++ Exercise23/Exercise23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userInput = Console.ReadLine();
+            File.WriteAllText("C:\\Users\\TTA_DEN_01\\Desktop\\C#_Course\\Projects\\EXERCISES\\Exercise23\\log.txt", userInput);
+            Console.WriteLine("Your logged number: " + File.ReadAllText("C:\\Users\\TTA_DEN_01\\Desktop\\C#_Course\\Projects\\EXERCISES\\Exercise23\\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
