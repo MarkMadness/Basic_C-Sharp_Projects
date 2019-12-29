020006818f6d78b840b8158c549fab7d3e27414d
Mark <1187productions@gmail.com>
2019-12-29 14:49:13 -0700

    Uploaded Exercise 26 files

---
+++ Exercise26/Exercise26/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise26
+{
+    public class NegativeException : Exception
+    {
+        public NegativeException()
+            : base() { }
+    }
+    public class ZeroException : Exception
+    {
+        public ZeroException()
+            : base() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the number of your age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What year were you born?");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                if (age == 0 || year == 0) throw new ZeroException();
+                if (age < 0 || year < 0) throw new NegativeException();
+
+                Console.WriteLine("You were born in " + year + ".");
+            }
+            catch(ZeroException)
+            {
+                Console.WriteLine("You entered a zero. You can't do that. Try again.");
+                Console.ReadLine();
+                return;
+            }
+            catch(NegativeException)
+            {
+                Console.WriteLine("You entered a negative input. You can't do that. Try again.");
+                Console.ReadLine();
+                return;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("There was an error in your answer. Try again.");
+                Console.ReadLine();
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
