6a6626448fd43349153ccb536ad45cd83c726637
Mark <1187productions@gmail.com>
2019-12-29 13:22:11 -0700

    Uploaded Exercise 25 files

---
+++ Exercise25/Exercise25/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise25
+{
+    public class Sentence
+    {
+        public string firstPart;
+        public string secondPart;
+        public Sentence(string firstPart) : this(firstPart, "Mark")
+        {
+
+        }
+        public Sentence(string first, string second)
+        {
+            firstPart = first;
+            secondPart = second;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(firstPart + secondPart);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string first = "My name is ";
+            var second = "Mark";
+            Sentence sen = new Sentence(first, second);
+            sen.PrintName();
+            Console.ReadLine();
+        }
+    }
+}
