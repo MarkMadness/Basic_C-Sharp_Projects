d66ee42b9f85c2e972ebce5bbfa3b3b4d8b1b820
Mark <1187productions@gmail.com>
2019-12-29 09:55:35 -0700

    Uploaded Exercise 24 files

---
+++ Exercise24/Exercise24/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine("The time right now is: " + time);
+            Console.WriteLine("Please, enter a number.");
+            double num = Convert.ToDouble(Console.ReadLine());
+            time = time.AddHours(num);
+            Console.WriteLine("The time in " + Convert.ToString(num) + " hours will be: " + time);
+            Console.ReadLine();
+        }
+    }
+}
